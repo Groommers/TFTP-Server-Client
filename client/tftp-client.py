b88c159ac78669aca750aee60e51dfe9200e9e96
GroupLambder <51271820+ESQNQRC@users.noreply.github.com>
2019-09-20 07:20:28 -0400

    ready

--- client/tftp-client.py
+++ client/tftp-client.py
@@ -1,17 +1,19 @@
 """tftp-client.
 Usage:
-  tftp-client.py get <filename> [[-s | -b ] --mode=<mode>]
-  tftp-client.py (-h | --help)
+  tftp-client.py 
+  <Filename>
+  <Mode>
+  <Type request>
 
 Options:
-  -h --help     Show this screen.
-  -s            Use python struct to build request.
-  -b            Use python bytearray to build request.
-  --mode=<mode> TFTP transfer mode : "netascii", "octet", or "mail"
+  <mode> TFTP transfer mode : "netascii", "octet", or "mail"
+  <Type request> TFTP type request : "RRQ" or "WRQ"
 """
 
 import socket
-from struct import pack
+import platform
+
+from os import remove
 
 """
 	opcode  operation
@@ -22,38 +24,6 @@ from struct import pack
 	 5     Error (ERROR)
 
 >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
-	  2 bytes     string    1 byte     string   1 byte
-	 ------------------------------------------------
-	| Opcode |  Filename  |   0  |    Mode    |   0  |
-	 ------------------------------------------------
-
-	Figure 5-1: RRQ/WRQ packet
-
-   The [[ Mode ]] field contains the
-
-   string "netascii", "octet", or "mail" (or any combination of upper
-   and lower case, such as "NETASCII", NetAscii", etc.)
-
-
->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
-	  2 bytes     2 bytes
-	 ---------------------
-	| Opcode |   Block #  |
-	 ---------------------
-
-	 Figure 5-3: ACK packet
-
->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
-	  2 bytes     2 bytes      n bytes
-	 ----------------------------------
-	| Opcode |   Block #  |   Data     |
-	 ----------------------------------
-
-	 Figure 5-2: DATA packet
->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 TFTP Formats
 
@@ -108,12 +78,13 @@ As shown above the protocol can be seen in action on the last 6 lines or so.
 
 ** Normal Termination ** - This excerpt is taken directly from `RFC 1350 <https://tools.ietf.org/html/rfc1350/>`_
   The end of a transfer is marked by a DATA packet that contains
-  between 0 and 511 bytes of data (i.e., Datagram length < 516).  This
+  between 0 and 511 bytes of data (i.e., Datagram length < 512).  This
   packet is acknowledged by an ACK packet like all other DATA packets.
   The host acknowledging the final DATA packet may terminate its side
   of the connection on sending the final ACK.
 
 """
+MAXSIZE = 508
 TERMINATING_DATA_LENGTH = 512
 TFTP_TRANSFER_MODE = b'netascii'
 
@@ -129,82 +100,7 @@ TFTP_MODES = {
 	'unknown': 0,
 	'netascii': 1,
 	'octet': 2,
-	'mail': 3}
-
-# Create a UDP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_address = ('localhost', 69)
-
-
-def send_rq(filename, mode):
-	"""
-	This function constructs the request packet in the format below.
-	Demonstrates how we can construct a packet using bytearray.
-
-		Type   Op #     Format without header
-
-			   2 bytes    string   1 byte     string   1 byte
-			   -----------------------------------------------
-		RRQ/  | 01/02 |  Filename  |   0  |    Mode    |   0  |
-		WRQ    -----------------------------------------------
-
-
-	:param filename:
-	:return:
-	"""
-	request = bytearray()
-	# First two bytes opcode - for read request
-	request.append(0)
-	request.append(1)
-	# append the filename you are interested in
-	filename = bytearray(filename.encode('utf-8'))
-	request += filename
-	# append the null terminator
-	request.append(0)
-	# append the mode of transfer
-	form = bytearray(bytes(mode, 'utf-8'))
-	request += form
-	# append the last byte
-	request.append(0)
-
-	print("Request", request)
-	sent = sock.sendto(request, server_address)
-
-
-def send_ack(ack_data, server):
-	"""
-	This function constructs the ack using the bytearray.
-	We dont change the block number cause when server sends data it already has
-	block number in it.
-
-			  2 bytes    2 bytes
-			 -------------------
-	  ACK   | 04    |   Block #  |
-			 --------------------
-	:param ack_data:
-	:param server:
-	:return:
-	"""
-	ack = bytearray(ack_data)
-	ack[0] = 0
-	ack[1] = TFTP_OPCODES['ack']
-	print(ack)
-	sock.sendto(ack, server)
-
-
-def server_error(data):
-	"""
-	We are checking if the server is reporting an error
-				2 bytes  2 bytes        string    1 byte
-			  ----------------------------------------
-	   ERROR | 05    |  ErrorCode |   ErrMsg   |   0  |
-			  ----------------------------------------
-	:param data:
-	:return:
-	"""
-	opcode = data[:2]
-	return int.from_bytes(opcode, byteorder='big') == TFTP_OPCODES['error']
-
+	'mail': 3} # not used
 
 # Map server error codes to messages [ Taken from RFC-1350 ]
 server_error_msg = {
@@ -218,48 +114,346 @@ server_error_msg = {
 	7: "No such user."
 }
 
+ip = 'localhost'
+
+# Create a UDP socket and open the port 69				 # socket.SOCK_DGRAM: type of the connector, DGRAM = UDP , STREAM = TCP
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)	 # socket.AF_INET: the domain of the connector, in this case, an IPv4 connector
+
+def send_rq(filename, mode, typeR):
+	"""
+	This function constructs the request packet in the format below.
+	Demonstrates how we can construct a packet using bytearray.
+
+		Type   Op #     Format without header
+
+			   2 bytes    string   1 byte     string   1 byte
+			   -----------------------------------------------
+		RRQ/  | 01/02 |  Filename  |   0  |    Mode    |   0  |
+		WRQ    -----------------------------------------------
+
+	"""
+	request = bytearray()
+	
+	# First two bytes opcode 
+	if (typeR == "RRQ"): # RRQ
+		request.append(0)
+		request.append(1)
+
+	else:                # WRQ
+		request.append(0)
+		request.append(2)
+
+	# append the filename you are interested in
+	filename = bytearray(filename.encode('utf-8'))
+	request += filename
+
+	# append the null terminator
+	request.append(0)
+
+	# append the mode of transfer
+	form = bytearray(bytes(mode, 'utf-8'))
+	request += form
+
+	# append the last byte
+	request.append(0)
+
+	server_address = (ip, 69)
+
+	print("Send request")
+	sent = sock.sendto(request, server_address)
+
+
+def send_ack(ack_data, server):
+	"""
+	This function constructs the ack using the bytearray.
+
+			  2 bytes    2 bytes
+			 -------------------
+	  ACK   | 04    |   Block #  |
+			 --------------------
+
+	"""
+	ack = bytearray(ack_data)
+
+	ack[0] = 0
+	ack[1] = TFTP_OPCODES['ack']
+
+	sock.sendto(ack, server)
+
+
+def server_error(data):
+	"""
+	We are checking if the server is reporting an error
+				2 bytes  2 bytes        string    1 byte
+			  ----------------------------------------
+	   ERROR | 05    |  ErrorCode |   ErrMsg   |   0  |
+			  ----------------------------------------
+
+	"""
+	opcode = data[:2]
+	return int.from_bytes(opcode, byteorder='big') == TFTP_OPCODES['error']
+
 
 def main():
-	filename = input("insert filename: ")
-	
-	mode = input("insert mode: ")
 
-	if mode.lower() not in TFTP_MODES.keys():
-		print("Unknown mode - defaulting to [ netascii ]")
-		mode = "netascii"
+	typeR = input("insert type request (RRQ or WRQ): ")
 
-	typeR = input("insert type request: ")
-
-	if typeR != "RRQ" and typeR != "WRQ":
+	if ( typeR != "RRQ" and typeR != "WRQ" ):
 		print("Unknown mode - defaulting to [ RRQ ]")
 		typeR = "RRQ"
 
-	if(typeR == "RRQ"): #if is RRQ
-		
+	mode = input("insert mode (octet or netascii): ")
+
+	if ( mode.lower() not in TFTP_MODES.keys() ):
+		print("Unknown mode - defaulting to [ netascii ]")
+		mode = "netascii"
+
+	filename = input("insert filename: ")
+	
+	server_address = (ip, 69)
+
+	if(typeR == "RRQ"):
 		# Send request
-		send_rq(filename, mode)
+		send_rq(filename, mode, typeR)
 
-		# Open file locally with the same name as that of the requested file from server
-		file = open(filename, "wb")
-		while True:
-			# Wait for the data from the server
-			data, server = sock.recvfrom(600)
+		if(mode == "octet"):
+			# Open file locally with the same name as that of the requested file from server
+			file = open(filename, "wb")
 
-			if server_error(data):
-				error_code = int.from_bytes(data[2:4], byteorder='big')
-				print(server_error_msg[error_code])
-				break
-			send_ack(data[0:4], server)
-			content = data[4:]
-			# print(f"Content : {content}")
-			file.write(content)
-			# print(f"## Data ##: {data[0:4]} : {len(data)}")
-			if len(data) < TERMINATING_DATA_LENGTH:
-				break
+			print("receiving file", filename)
+
+			while True:
+				# Wait for the data from the server
+				data, server = sock.recvfrom(600)
+
+				if (server_error(data)):
+					error_code = int.from_bytes(data[2:4], byteorder='big')
+
+					file.close()
+
+					if (error_code == 1):
+						remove(filename)
+
+					print(server_error_msg[error_code])
+					break
+
+				send_ack(data[0:4], server)
+
+				content = data[4:] 
+				file.write(content)
+
+				if (len(data) < TERMINATING_DATA_LENGTH):
+					print("Transfer complete")
+
+					file.close()
+
+					break
+
+		elif (mode == "netascii"):
+			# Open file locally with the same name as that of the requested file from server
+			file = open(filename, "w")
+			while True:
+				# Wait for the data from the server
+				data, server = sock.recvfrom(600)
+
+				if (server_error(data)):
+					error_code = int.from_bytes(data[2:4], byteorder='big')
+
+					file.close()
+
+					if (error_code == 1):
+						remove(filename)
+
+					print(server_error_msg[error_code])
+					break
+
+				send_ack(data[0:4], server)
+
+				content = data[4:].decode()
+				
+				# Handle the end of line control char
+				osType = platform.system()
+
+				if (osType == 'Linux') :
+					for item in content :
+						if ( item != '\r' ):
+							file.write(item)
+
+				elif (osType == 'Windows') :
+					file.write(content)
+
+				if len(data) < TERMINATING_DATA_LENGTH:
+					print("Transfer complete")
+
+					file.close()
+
+					break
+
+		else: # mail is only for WRQ but for readability it is placed as the same as for netascii
+
+			file = open(filename, "w")
+
+			print("receiving file", filename)
+
+			while True:
+				# Wait for the data from the server
+				data, server = sock.recvfrom(600)
+
+				if (server_error(data)):
+
+					error_code = int.from_bytes(data[2:4], byteorder='big')
+
+					print(server_error_msg[error_code])
+					break
+
+				send_ack(data[0:4], server)
+				
+				content = data[4:] 
+				file.write(content)
+
+				if (len(data) < TERMINATING_DATA_LENGTH):
+
+					print("Transfer complete")
+
+					file.close()
+
+					break
 
 	else: #if is WRQ
-		pass
 
+		if(mode == "octet"):
+
+			try:
+				file = open(filename, "rb")
+
+			except:
+				print("ERROR, File not found")
+				exit()
+
+			# Send request
+			send_rq(filename, mode, typeR)
+
+			print("Sending file")
+
+			count = 0 # Block number
+
+			while True: # Send the file in blocks
+
+				block = file.read(MAXSIZE) # read the block
+
+				if not block:
+
+					if (count == 0):
+
+						data = bytearray()
+
+						data.append(0)
+						data.append(3)
+
+						b = bytearray(count.to_bytes(2, 'big'))
+						data += b
+
+						sent = sock.sendto(data, server_address)
+
+						data, server_address = sock.recvfrom(MAXSIZE)
+						
+						while( data[0] != 0 and data[1] != 4 and data[2] != b[0] and data[3] != b[1] ): # Wait for ACK
+							data, server_address = sock.recvfrom(MAXSIZE)
+
+					print("Transfer complete")
+
+					file.close()
+					break
+
+				data = bytearray()
+
+				data.append(0)
+				data.append(3)
+
+				b = bytearray(count.to_bytes(2, 'big'))
+				data += b
+
+				data += block
+
+				sent = sock.sendto(data, server_address) # Send the data
+
+				data, server_address = sock.recvfrom(MAXSIZE)	
+
+				while( data[0] != 0 and data[1] != 4 and data[2] != b[0] and data[3] != b[1] ): # Wait for ACK
+					data, server_address = sock.recvfrom(MAXSIZE)
+
+				if (count + 1 >= 60000):
+					count = 0
+
+				else:
+					count += 1
+
+
+		elif(mode == "netascii"):
+			try:
+				file = open(filename, "r")
+			except:
+				print("ERROR, File not found")
+				exit()
+
+			# Send request
+			send_rq(filename, mode, typeR)
+
+			print("Sending file")
+
+			count = 0 # Block number
+
+			while True: # Send the file in blocks
+
+			    ## Note: Change reading to char  by char if want to put /r or not
+				block = file.read(MAXSIZE) # read the block
+
+				if not block:
+
+					if (count == 0):
+						
+						data = bytearray()
+
+						data.append(0)
+						data.append(3)
+
+						b = bytearray(count.to_bytes(2, 'big'))
+						data += b
+
+						sent = sock.sendto(data, server_address)
+
+						data, server_address = sock.recvfrom(MAXSIZE)
+						
+						while( data[0] != 0 and data[1] != 4 and data[2] != b[0] and data[3] != b[1] ): # Wait for ACK
+							data, server_address = sock.recvfrom(MAXSIZE)
+
+					print("Transfer complete")
+
+					file.close()
+					break
+
+				data = bytearray()
+
+				data.append(0)
+				data.append(3)
+
+				b = bytearray(count.to_bytes(2, 'big'))
+				data += b
+
+				data += block.encode()
+
+				sent = sock.sendto(data, server_address) # Send the data
+
+				data, server_address = sock.recvfrom(MAXSIZE)
+
+				while( data[0] != 0 and data[1] != 4 and data[2] != b[0] and data[3] != b[1] ): # Wait for ACK
+					data, server_address = sock.recvfrom(MAXSIZE)
+
+				if (count + 1 >= 60000):
+					count = 0
+
+				else:
+					count += 1
 
 if __name__ == '__main__':
 	main()
